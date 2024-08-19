1fba6e4872917e824fe7ca4d606fc2caf6e0a543
BlackBadger777 <93954783+BlackBadger777@users.noreply.github.com>
2024-08-19 14:57:40 +0200

    Delete scripts directory

--- scripts/booming-blade.mjs
+++
@@ -1,5 +0,0 @@
-Hooks.on("babonus.applyOptionalBonus", async function(bonus, roller, target, config) {
-  if(!(bonus.parent instanceof Item) || (bonus.item.name !== "Booming Blade")) return;
-  const actor = (roller instanceof Item) ? roller.actor : roller;
-game.dfreds.effectInterface.toggleEffect("Booming Blade Move Damage")
-});
